1a755f096d07b0297897c02dfff30fbc5e9fe4eb
Seth <sethkujawa@gmail.com>
2020-11-04 13:51:09 -0500

    Updates

---
+++ precache-manifest.6b1c01946581441f46335af592c5e1b2.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f3af30e51ce80944adaba51ba32d002",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e27291e258ad40a6672c",
+    "url": "/static/css/2.91542726.chunk.css"
+  },
+  {
+    "revision": "e04348305539a6b894a5",
+    "url": "/static/css/main.8f726e74.chunk.css"
+  },
+  {
+    "revision": "e27291e258ad40a6672c",
+    "url": "/static/js/2.df681863.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.df681863.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e04348305539a6b894a5",
+    "url": "/static/js/main.420268d5.chunk.js"
+  },
+  {
+    "revision": "87125e563b71d9dcb45e",
+    "url": "/static/js/runtime-main.74d97b6f.js"
+  },
+  {
+    "revision": "69db09ac5ef6b8346479b74d19b42eb4",
+    "url": "/static/media/akpsi_logo.69db09ac.svg"
+  },
+  {
+    "revision": "8bb918d303fbc3611e75446a274bf845",
+    "url": "/static/media/akpsiot_sticker.8bb918d3.svg"
+  },
+  {
+    "revision": "a770b6797b68e3f8920e473eb824bac0",
+    "url": "/static/media/loader-big.a770b679.gif"
+  },
+  {
+    "revision": "cf050959552e373a0280b15eeca5655f",
+    "url": "/static/media/plus.cf050959.png"
+  },
+  {
+    "revision": "c625dfc485ce3444fe2036d02f120bca",
+    "url": "/static/media/reviews.c625dfc4.png"
+  },
+  {
+    "revision": "12f0820c451bdc75f4d1ef97732bf6e8",
+    "url": "/static/media/rw-widgets.12f0820c.woff"
+  },
+  {
+    "revision": "792dcd18baf5f544aabcad1883d673c2",
+    "url": "/static/media/rw-widgets.792dcd18.svg"
+  },
+  {
+    "revision": "bc7c4a59f924cf037aad6e1f9edba366",
+    "url": "/static/media/rw-widgets.bc7c4a59.eot"
+  },
+  {
+    "revision": "eceddf474df95d8d4a7e316668c3be85",
+    "url": "/static/media/rw-widgets.eceddf47.ttf"
+  },
+  {
+    "revision": "359f2862cc97168b03e94c92b6c0bba4",
+    "url": "/static/media/stock_1.359f2862.jpg"
+  },
+  {
+    "revision": "6b6f4069f1cef08c532a0986da48a9d5",
+    "url": "/static/media/stock_2.6b6f4069.jpg"
+  },
+  {
+    "revision": "29b6dbd0f8da2625d361627202a64a7e",
+    "url": "/static/media/stock_3.29b6dbd0.jpg"
+  },
+  {
+    "revision": "e98bf002df18fe7f50bc96e1bdd3a642",
+    "url": "/static/media/stock_4.e98bf002.jpg"
+  },
+  {
+    "revision": "7592ca50fc56b281699293dc98ccbf6d",
+    "url": "/static/media/stock_5.7592ca50.jpg"
+  },
+  {
+    "revision": "22216b737035761928bbbf9fa959fc32",
+    "url": "/static/media/stock_6.22216b73.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/omegathetabase.org/precache-manifest.a6361aedf61aa3e690a8eb59843eaa7f.js"
+  "/precache-manifest.6b1c01946581441f46335af592c5e1b2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/omegathetabase.org/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
